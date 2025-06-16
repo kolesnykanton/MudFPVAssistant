6f1270ce63eaadc4ec23325fd299dcbf595d8e7f
Anton <kolesnyk.antony@gmail.com>
2025-06-16 16:43:32 +0200

    separate settings classes

--- Models/UserApiKeys.cs
+++ Models/UserApiKeys.cs
@@ -1,14 +1,4 @@
-﻿using System.Text.Json.Serialization;
-
-namespace MudFPVAssistant.Models;
-
-public class UserSettings
-{
-    [JsonPropertyName("id")]
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    public string? Id { get; set; }
-    public UserApiKeys ApiKeys { get; set; } = new();
-}
+﻿namespace MudFPVAssistant.Models;
 
 public class UserApiKeys
 {
---
+++ Models/UserSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MudFPVAssistant.Models;
+
+public class UserSettings
+{
+    [JsonPropertyName("id")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Id { get; set; }
+    public UserApiKeys ApiKeys { get; set; } = new();
+}
